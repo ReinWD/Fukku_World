3d0e13762bd1a2a78a715717d960abdd958bdd2a
ReinWD <zhangw1998419@qq.com>
2016-11-24 00:09:44 +0800

    GaoShiHui 1st Day

--- Android Projs/GaoShiHui/app/src/main/java/com/example/gaoshihui/LoginActivity.java
+++ Android Projs/GaoShiHui/app/src/main/java/com/example/gaoshihui/LoginActivity.java
@@ -2,6 +2,12 @@ package com.example.gaoshihui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class LoginActivity extends Activity {
 
@@ -9,5 +15,28 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //declare
+        Button buttonLogin, buttonSignIn;
+        EditText editTextEmail, editTextPassword;
+        TextView textViewForgottenPassword;
+
+        editTextEmail = (EditText) findViewById(R.id.login_account);
+        editTextPassword = (EditText) findViewById(R.id.login_password);
+
+        buttonLogin=(Button)findViewById(R.id.login_button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        buttonSignIn=(Button)findViewById(R.id.login_button_sign_in);
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file=new File("..\\Accounts\\");
+            }
+        });
+
     }
 }
